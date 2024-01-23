fc413a31f0da81601ee95fb3408cfc7fcf6b476b
HernanRodriguez1 <66162160+HernanRodriguez1@users.noreply.github.com>
2024-01-23 17:36:22 -0500

    Update SQLi_Sleeps2.py

--- SQLi_Sleeps2.py
+++ SQLi_Sleeps2.py
@@ -8,7 +8,7 @@ def perform_request(url, data, cookie):
 
     try:
         response = requests.get(url_with_data, cookies={'cookie': cookie} if cookie else None)
-        response.raise_for_status()  # Lanza una excepción si la respuesta indica un error HTTP
+        response.raise_for_status() 
     except requests.exceptions.RequestException as e:
         return False, url_with_data, time.time() - start_time, str(e)
 
@@ -21,19 +21,16 @@ def main():
     parser.add_argument("-c", "--cookie", help="Cookie a incluir en la petición GET.")
     args = parser.parse_args()
 
-    # Lee las URLs y datos desde los archivos
     with open(args.urls) as file:
         urls = file.read().splitlines()
 
     with open(args.data) as file:
         data = file.read().splitlines()
 
-    # Recorre la lista de URLs y datos
     for url in urls:
         for d in data:
             success, url_with_data, response_time, error_message = perform_request(url, d, args.cookie)
 
-            # Imprime la URL y el tiempo de respuesta en color verde o rojo
             if success and response_time <= 20:
                 print("\033[1;32mURL {} - {:.2f} segundos\033[0m".format(url_with_data, response_time))
             else:
